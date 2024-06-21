0b26c836d81c97846209b34ee3d1d3b9d5f3ea2a
Edward Chhun <edwardchhun3@gmail.com>
2024-06-20 23:07:36 -0700

    add files

---
+++ opencv.py
@@ -0,0 +1,31 @@
+import pathlib
+import cv2
+
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
+
+clf = cv2.CascadeClassifier(str(cascade_path))
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    _, frame = camera.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    for (x,y,width,height) in faces:
+        cv2.rectangle(frame, (x,y), (x+width,y+height), (255, 255, 0), 2)
+        
+    cv2.imshow("Faces", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+        
+camera.release()
+cv2.destroyAllWindows()
+
+        
