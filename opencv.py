32740aabb444cf25b649fdc69081bd0ebb1d8058
Edward Chhun <edwardchhun3@gmail.com>
2024-06-20 23:32:03 -0700

    Flipped Camera

--- opencv.py
+++ opencv.py
@@ -7,9 +7,15 @@ clf = cv2.CascadeClassifier(str(cascade_path))
 
 camera = cv2.VideoCapture(0)
 
+if not camera.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
 while True:
     _, frame = camera.read()
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     faces = clf.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -19,11 +25,15 @@ while True:
     )
     
     for (x,y,width,height) in faces:
-        cv2.rectangle(frame, (x,y), (x+width,y+height), (255, 255, 0), 2)
+        cv2.rectangle(frame, (x,y), (x+width,y+height), (255, 255, 0), 2)    
         
+    # This just flips the frame
+    frame = cv2.flip(frame, 1)
+
     cv2.imshow("Faces", frame)
     if cv2.waitKey(1) == ord("q"):
         break
+    
         
 camera.release()
 cv2.destroyAllWindows()
